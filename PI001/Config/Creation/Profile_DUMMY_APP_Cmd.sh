0b277be746ccfebdd44afea72a6663a04cbe10b7
Sugumar02 <suraja@primark.co.uk>
2017-03-08 15:48:57 +0530

    Update
    Update

--- PI001/Config/Creation/Profile_DUMMY_APP_Cmd.sh
+++ PI001/Config/Creation/Profile_DUMMY_APP_Cmd.sh
@@ -1,7 +1,7 @@
 broker=$1
 eg=$2
 mqsicreateconfigurableservice $broker -c MonitoringProfiles -o Dummy_Flow
-mqsichangeproperties $broker -c MonitoringProfiles -o Dummy_Flow -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_DUMMY_APP.xml
+mqsichangeproperties $broker -c MonitoringProfiles -o Dummy_Flow -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_DUMMY_APP_Dummy_Flow.xml
 mqsichangeproperties $broker -c MonitoringProfiles -o Dummy_Flow -n useParserNameInMonitoringPayload -v TRUE 
 mqsichangeflowmonitoring $broker -e Jenkins_Test -k DUMMY_APP -f Dummy_Flow -m Dummy_Flow
 mqsichangeflowmonitoring $broker -e Jenkins_Test -k DUMMY_APP -f Dummy_Flow -s Dummy_Input.terminal.out -i enable 
