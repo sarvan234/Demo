c313292ab871c028e481a4302515dee87c45a271
Sugumar02 <suraja@primark.co.uk>
2017-03-08 16:02:39 +0530

    Update
    Update

--- PI001/Config/Creation/Profile_DUMMY_APP_Cmd.sh
+++ PI001/Config/Creation/Profile_DUMMY_APP_Cmd.sh
@@ -1,12 +1,13 @@
 broker=$1
 eg=$2
-mqsicreateconfigurableservice $broker -c MonitoringProfiles -o Dummy_Flow
-mqsichangeproperties $broker -c MonitoringProfiles -o Dummy_Flow -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_DUMMY_APP_Dummy_Flow.xml
-mqsichangeproperties $broker -c MonitoringProfiles -o Dummy_Flow -n useParserNameInMonitoringPayload -v TRUE 
-mqsichangeflowmonitoring $broker -e Jenkins_Test -k DUMMY_APP -f Dummy_Flow -m Dummy_Flow
-mqsichangeflowmonitoring $broker -e Jenkins_Test -k DUMMY_APP -f Dummy_Flow -s Dummy_Input.terminal.out -i enable 
-mqsichangeflowmonitoring $broker -e Jenkins_Test -k DUMMY_APP -f Dummy_Flow -s Dummy_Output.terminal.in -i enable 
-mqsichangeflowmonitoring $broker -e Jenkins_Test -k DUMMY_APP -f Dummy_Flow -c active
+mqsicreateconfigurableservice $broker -c MonitoringProfiles -o Dummy_Flow1
+mqsichangeproperties $broker -c MonitoringProfiles -o Dummy_Flow1 -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_DUMMY_APP_Dummy_Flow.xml
+mqsichangeproperties $broker -c MonitoringProfiles -o Dummy_Flow1 -n useParserNameInMonitoringPayload -v TRUE 
+mqsichangeflowmonitoring $broker -e default -k DUMMY_APP -f Dummy_Flow -m Dummy_Flow1
+mqsichangeflowmonitoring $broker -e default -k DUMMY_APP -f Dummy_Flow -s Dummy_Input.terminal.out -i enable 
+mqsichangeflowmonitoring $broker -e default -k DUMMY_APP -f Dummy_Flow -s Dummy_Output.terminal.in -i enable 
+mqsichangeflowmonitoring $broker -e default -k DUMMY_APP -f Dummy_Flow -c active
+
 
 
 
