ff76d2da332c2fcd4ffdde7e108c5ee083c8e20b
Sugumar02 <suraja@primark.co.uk>
2017-04-24 10:42:21 +0530

    Update
    Update

--- PI172/Config/Creation/Profile_ORACLE_CoA_Cmd.sh
+++ PI172/Config/Creation/Profile_ORACLE_CoA_Cmd.sh
@@ -1,7 +1,7 @@
 broker=$1
 mqsicreateconfigurableservice $broker -c MonitoringProfiles -o ORACLE_ESB_CoA
 
-mqsichangeproperties $broker -c MonitoringProfiles -o ORACLE_ESB_CoA -n profileProperties -p /var/mqsi/monitoringprofiles/ORACLE_ESB_CoA_PMF.xml
+mqsichangeproperties $broker -c MonitoringProfiles -o ORACLE_ESB_CoA -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_ORACLE_CoA_ORACLE_ESB_CoA_PMF.xml
 
 mqsichangeproperties $broker -c MonitoringProfiles -o ORACLE_ESB_CoA -n useParserNameInMonitoringPayload -v TRUE 
 
@@ -19,7 +19,7 @@ mqsichangeflowmonitoring $broker -e OracleFinancial -k ORACLE_CoA -f esb.primark
 
 mqsicreateconfigurableservice $broker -c MonitoringProfiles -o ESB_PIAT_CoA_ACCOUNTS
 
-mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_ACCOUNTS -n profileProperties -p /var/mqsi/monitoringprofiles/ESB_PIAT_CoA_ACCOUNTS_CMF.xml
+mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_ACCOUNTS -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_ORACLE_CoA_ESB_PIAT_CoA_ACCOUNTS_CMF.xml
 
 mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_ACCOUNTS -n useParserNameInMonitoringPayload -v TRUE 
 
@@ -37,7 +37,7 @@ mqsichangeflowmonitoring $broker -e OracleFinancial -k ORACLE_CoA -f esb.primark
 
 mqsicreateconfigurableservice $broker -c MonitoringProfiles -o ESB_PIAT_CoA_PROJECTS
 
-mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_PROJECTS -n profileProperties -p /var/mqsi/monitoringprofiles/ESB_PIAT_CoA_PROJECTS_CMF.xml
+mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_PROJECTS -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_ORACLE_CoA_ESB_PIAT_CoA_PROJECTS_CMF.xml
 
 mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_PROJECTS -n useParserNameInMonitoringPayload -v TRUE 
 
@@ -56,7 +56,7 @@ mqsichangeflowmonitoring $broker -e OracleFinancial -k ORACLE_CoA -f esb.primark
 
 mqsicreateconfigurableservice $broker -c MonitoringProfiles -o ESB_PIAT_LOCATIONS
 
-mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_LOCATIONS -n profileProperties -p /var/mqsi/monitoringprofiles/ESB_PIAT_CoA_LOCATIONS_CMF.xml
+mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_LOCATIONS -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_ORACLE_CoA_ESB_PIAT_CoA_LOCATIONS_CMF.xml
 
 mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_LOCATIONS -n useParserNameInMonitoringPayload -v TRUE
 
@@ -74,7 +74,7 @@ mqsichangeflowmonitoring $broker -e OracleFinancial -k ORACLE_CoA -f esb.primark
 
 mqsicreateconfigurableservice $broker -c MonitoringProfiles -o ESB_PIAT_CoA_COSTCENTRES
 
-mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_COSTCENTRES -n profileProperties -p /var/mqsi/monitoringprofiles/ESB_PIAT_CoA_COSTCENTRES_CMF.xml
+mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_COSTCENTRES -n profileProperties -p /var/mqsi/monitoringprofiles/Profile_ORACLE_CoA_ESB_PIAT_CoA_COSTCENTRES_CMF.xml
 
 mqsichangeproperties $broker -c MonitoringProfiles -o ESB_PIAT_CoA_COSTCENTRES -n useParserNameInMonitoringPayload -v TRUE 
 
