fc1e1e87a0cfb55056c45282b629072036d63ff5
IEDU1WVDSQL002$@primark.local <IEDU1WVDSQL002$@primark.local>
2018-06-05 10:50:51 +0100

    Sugumar Raja G: Commiting PI003 Script Files

--- PI003/Scripts/Creation/Creation_Scheduler_MFT_DUMMY_PreProd.sh
+++ PI003/Scripts/Creation/Creation_Scheduler_MFT_DUMMY_PreProd.sh
@@ -1,2 +1,2 @@
 cd /opt/mqm/bin
-./fteCreateTransfer -sa MFT01 -sm MFT01 -da MFT01 -dm MFT01 -sd leave -t text -de overwrite -ss 2014-09-29T23:45 -oi days -jn MFT_DUMMY -md jobname=MFT_DUMMY -md destination_support_group=NTSS_Support_Group -md source_support_group=NTSS_Support_Group -md severity=2 -dd /home/MQFTE_LINUX /home/TEST/test/*.txt
+./fteCreateTransfer -sa MFT01 -sm MFT01 -da MFT01 -dm MFT01 -sd leave -de overwrite -ss 2014-09-29T23:45 -oi days -of 5 -jn MFT_DUMMY -dd /home/MQFTE_LINUX /home/TEST/test/*.txt -p MFT01 -presrc DUMMY.sh 
