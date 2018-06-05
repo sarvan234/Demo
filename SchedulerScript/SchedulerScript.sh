fe60936d7cdb7a3b2f344a048752e2620447a72b
Sugumar02 <suraja@primark.co.uk>
2018-06-05 15:07:33 +0530

    SchedulerScript update

--- SchedulerScript/SchedulerScript.sh
+++ SchedulerScript/SchedulerScript.sh
@@ -1,2 +1,2 @@
 cd /opt/mqm/bin
-./fteCreateTransfer -sa SourceAgent -sm SourceQmanager -da DestAgent -dm DestQmanager -sd leave -t text -de overwrite -ss SchDateTime -oi Interval -jn JobName -md jobname=JobName -md destination_support_group=NTSS_Support_Group -md source_support_group=NTSS_Support_Group -md severity=Severity -dd DestPath SourcePath
+./fteCreateTransfer -sa SourceAgent -sm SourceQmanager -da DestAgent -dm DestQmanager -sd SourceDisp -de DestExists -ss SchDateTime -oi Interval -of OccurFreq -jn JobName -dd DestPath SourcePath -p ConfigQmanager SrcDestCall 
